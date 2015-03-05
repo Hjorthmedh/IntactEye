26463e5ca6aff61665592d3becdaacbf90dbfdbc
Johannes Hjorth <jjjh2@cam.ac.uk>
2015-03-05 11:38:12 +0000

    Updated polar plot of optic disk, bigger font

--- locatingOpticDiskRedux.m
+++ locatingOpticDiskRedux.m
@@ -5,6 +5,14 @@ clear all, close all
 expList = {'D5.6', 'Ret22.2', 'Ret22.3', 'Ret22.6', 'Ret22.9', 'Ret32.2'};
 expPath = '/Users/hjorth/DATA/ReberRetina';
 
+expColour = [166,206,227
+             31,120,180
+             178,223,138
+             51,160,44
+             251,154,153
+             227,26,28]/255;
+
+
 ES = EyeSphere([],[],[]);
 
 for i = 1:numel(expList)
@@ -41,25 +49,47 @@ end
 figure
 
 v = linspace(0,2*pi,100);
-p2 = polar(v,5*ones(1,100),'y-');
+p2 = polar(v,3*ones(1,100),'y-');
 hold on
+
+spokesV = [0 180; 30 210; 60 240; 90 270; 120 300; 150 330]'*pi/180;
+spokesV2 = linspace(0,2*pi,100);
+
+plot(3*cos(spokesV),3*sin(spokesV),'-','color',0.6*[1 1 1])
+plot(1*cos(spokesV2),1*sin(spokesV2),'-','color',0.6*[1 1 1])
+plot(2*cos(spokesV2),2*sin(spokesV2),'-','color',0.6*[1 1 1])
+plot(3*cos(spokesV2),3*sin(spokesV2),'-','color',0.6*[1 1 1])
+
+% Plot the circles and lines in another colour, since I did not
+% find a way to directly change the colours
 
 ODcentreIE(:,1) = pi/2 + ODcentreIE(:,1);
 
 ODcentreIE(:,1) = pi - ODcentreIE(:,1);
 
-p = polar([ODcentreIE(:,2), ODcentreRS(:,2)]', [ODcentreIE(:,1) ODcentreRS(:,1)]','-')
-mIE = polar(ODcentreIE(:,2),ODcentreIE(:,1),'k.');
-mRS = polar(ODcentreRS(:,2),ODcentreRS(:,1),'kv');
+for i = 1:numel(expList)
+  p(i) = polar([ODcentreIE(i,2), ODcentreRS(i,2)]', [ODcentreIE(i,1) ODcentreRS(i,1)]');
+  set(p(i),'color',expColour(i,:),'linewidth',2)
+  mIE = polar(ODcentreIE(i,2),ODcentreIE(i,1),'.');
+  set(mIE,'color',expColour(i,:));
+  mRS = polar(ODcentreRS(i,2),ODcentreRS(i,1),'v');
+  set(mRS,'color',expColour(i,:));
+  set(mIE,'markersize',40)
+  set(mRS,'markersize',12)
 
-set(mIE,'markersize',20)
-set(mRS,'markersize',6)
+end
+  
 
 delete(p2)
 legend(p,expList)
 
 
 axis equal
-set(gca,'fontsize',15)
+set(gca,'fontsize',20)
+
+th = findall(gcf,'Type','text');
+for i = 1:length(th),
+      set(th(i),'FontSize',18)
+end
 
 saveas(gcf,'FIGS/optic-disk-comparison-RS-IE.pdf','pdf')
